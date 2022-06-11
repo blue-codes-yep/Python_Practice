00d8976246c5a4f7368307cc5a5ac02f0a22aa2c
linzblue1 <linzblue5@gmail.com>
2022-06-11 18:58:58 -0500

    Working with libraries, and apis.

---
+++ exercises/itunes.py
@@ -0,0 +1,22 @@
+import json  # Can import json to make json more readiable.
+import sys
+import requests
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+
+response = requests.get(
+    "https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+
+# Print out the contents of the search
+
+print(json.dumps(response.json(), indent=2))
+
+
+# Grabs the track name from the results key in search api for itunes.
+
+o = response.json()
+
+for result in o["results"]:
+    print(result["trackName"])
