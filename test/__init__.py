e5c0255642c5ee7fc5c3f839b698e1ed472fb288
linzblue1 <linzblue5@gmail.com>
2022-06-18 22:38:14 -0500

    Testing, and minor changes

---
+++ test/__init__.py
@@ -0,0 +1,3 @@
+# Creating a file called init of having the affect of treating the test folder as a package (As in say you have multiple test then it will run the whole folder as a package.)
+
+# So you then could run something like 'pytest test' and it would run the whole folder.
